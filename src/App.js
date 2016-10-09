7fe6f3d93a617bc5f52cbb02fb39e63a4958624d
jurgob <jurgo.boemo@gmail.com>
2016-10-09 18:23:41 +0100

    add some padding

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Forecast from './components/Forecast'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{padding:"10px"}} >
         <Forecast />
       </div>
     );
